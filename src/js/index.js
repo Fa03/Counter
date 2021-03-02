ec65856d3567cafbea39cb52f9ff2aa8f1b731dd
Fabian Brenes <fabianbrenes@ice.co.cr>
2021-03-02 02:41:07 +0000

    Final

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,47 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
-//include bootstrap npm library into the bundle
 import "bootstrap";
 
-//include your index.scss file into the bundle
 import "../styles/index.scss";
 
-//import your own components
-import { Home } from "./component/home.js";
+function Counter(props) {
+	return (
+		<div className="Principal">
+			<div className="ImagenReloj">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="CuartaPosicion">{props.numeroCuatro % 10}</div>
+			<div className="TerceraPosicion">{props.numeroTres % 10}</div>
+			<div className="SegundaPosicion">{props.numeroDos % 10}</div>
+			<div className="PrimerPosicion">{props.numeroUno % 10}</div>
+		</div>
+	);
+}
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+Counter.propTypes = {
+	numeroCuatro: PropTypes.number,
+	numeroTres: PropTypes.number,
+	numeroDos: PropTypes.number,
+	numeroUno: PropTypes.number
+};
+
+let contador = 0;
+setInterval(function() {
+	const cuarto = Math.floor(contador / 1000);
+	const tercero = Math.floor(contador / 100);
+	const segundo = Math.floor(contador / 10);
+	const primero = Math.floor(contador / 1);
+	console.log(cuarto, tercero, segundo, primero);
+	contador++;
+	ReactDOM.render(
+		<Counter
+			numeroUno={primero}
+			numeroDos={segundo}
+			numeroTres={tercero}
+			numeroCuatro={cuarto}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
